e99492315759a7f5f9d3011624d6389e8d8d2223
Andre Specht <andrespecht@outlook.com>
2022-02-14 18:53:36 -0600

    Web Class 7

---
+++ Class files/Web development/Week 3/assets/script/code.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// My first JavaScript file
+
+let variable = 'value';
+const constant = 'another value';
+
+let num_1 = 123;
+let num_2 = 456;
+
+let sum = num_1 + num_2;
+let sub = num_1 - num_2;
+let mult = num_1 * num_2;
+let div = num_2 / num_1;
